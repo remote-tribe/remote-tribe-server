4ddc986797253a1bea1e6caf5e62b421044ad2d4
valyy151 <marin.valenta1@gmail.com>
2023-04-13 13:27:38 +0200

    User can now update his profile

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -2,140 +2,132 @@ const express = require('express')
 const router = express.Router()
 const app = express()
 
-// require cloudinary
-const fileUploader = require('../config/cloudinary.config')
+// // require cloudinary
+// const fileUploader = require('../config/cloudinary.config')
 
-//require Article model
-const Article = require('../models/Article.model')
+// //require Article model
+// const Article = require('../models/Article.model')
 
+// //READ: list of articles from community
+// router.get('/community/articles', (req, res, next) => {
+// 	Article.find()
+// 		.then((articleArr) => {
+// 			const data = {
+// 				article: articleArr,
+// 			}
+// 			res.json(data)
+// 		})
+// 		.catch((err) => {
+// 			console.log('error getting list of articles', err)
+// 			res.status(500).json({
+// 				message: 'error getting list of articles',
+// 				error: err,
+// 			})
+// 		})
+// })
 
-//READ: list of articles from community
-router.get("/community/articles", (req, res, next) => {
-    Article.find()
-        .then(articleArr => {
-            const data = {
-                article: articleArr
-            }
-            res.json(data);
-        })
-        .catch(err => {
-            console.log("error getting list of articles", err);
-            res.status(500).json({
-                message: "error getting list of articles",
-                error: err
-            });
-        })
-})
+// // CREATE: article
 
-// CREATE: article
+// // router.post('/community/articles/:articleId', fileUploader.single('article-image'), (req, res, next) => {
+// // 	const { title, content } = req.body
+// // 	const userId = req.params
+// // 	const { imageUrl } = req.file.path || ''
 
-router.post("/community/articles/:articleId", fileUploader.single('article-image'), (req, res, next) => {
+// // 	Article.create({ author, title, content, imageUrl, createdAt: new Date() })
+// // 		.then((responseFromDB) => {
+// // 			res.status(201).json(responseFromDB)
+// // 		})
+// // 		.catch((err) => {
+// // 			console.log('error creating article', err)
+// // 			res.status(500).json({
+// // 				message: 'error creating article',
+// // 				error: err,
+// // 			})
+// // 		})
+// // })
 
-    const { title, content } = req.body;
-    const userId = req.params;
-    const { imageUrl } = req.file.path || "";
+// // Article.create({ author, title, content, imageUrl, createdAt: new Date() })
+// // 	.then((responseFromDB) => {
+// // 		res.status(201).json(responseFromDB)
+// // 	})
+// // 	.catch((err) => {
+// // 		console.log('error creating article', err)
+// // 		res.status(500).json({
+// // 			message: 'error creating article',
+// // 			error: err,
+// // 		})
+// // 	})
 
-    Article.create({ author: userId, title, content, imageUrl, createdAt: new Date() })
-        .then(responseFromDB => {
-            res.status(201).json(responseFromDB);
-        })
-        .catch(err => {
-            console.log("error creating article", err);
-            res.status(500).json({
-                message: "error creating article",
-                error: err
-            });
-        })
+// //READ: article details
+// router.get('/community/article/:articleId', (req, res, next) => {
 
-})
+// 	Article.findById(articleId)
+// 		.populate('author', 'comments')
+// 		.then((articleDetails) => {
+// 			res.json(articleDetails)
+// 		})
+// 		.catch((err) => {
+// 			console.log('error getting the article', err)
+// 			res.status(500).json({
+// 				message: 'error getting the article',
+// 				error: err,
+// 			})
+// 		})
+// })
 
-		Article.create({ author: userId, title, content, imageUrl, createdAt: new Date() })
-			.then((responseFromDB) => {
-				res.status(201).json(responseFromDB)
-			})
-			.catch((err) => {
-				console.log('error creating article', err)
-				res.status(500).json({
-					message: 'error creating article',
-					error: err,
-				})
-			})
-	},
-)
+// //DELETE article
+// router.delete('/community/article/:articleId', (req, res, next) => {
+// 	const { articleId } = req.params
 
-//READ: article details
-router.get("/community/article/:articleId", (req, res, next) => {
+// 	Article.findByIdAndDelete(articleId)
+// 		.then(() => res.json('this article was deleted'))
+// 		.catch((error) => next(error))
+// })
 
-	Article.findById(articleId)
-		.populate('author', 'comments')
-		.then((articleDetails) => {
-			res.json(articleDetails)
-		})
-		.catch((err) => {
-			console.log('error getting the article', err)
-			res.status(500).json({
-				message: 'error getting the article',
-				error: err,
-			})
-		})
-})
+// Article.findByIdAndDelete(articleId)
+// 	.then(() => res.json('this article was deleted'))
+// 	.catch((error) => next(error))
 
-//DELETE article
-router.delete('/community/article/:articleId', (req, res, next) => {
-    const { articleId } = req.params;
+// // UPDATE:  article
+// router.put('/community/article/:articleId', fileUploader.single('post-image'), (req, res, next) => {
+// 	const { articleId } = req.params
 
-    Article.findByIdAndDelete(articleId)
-        .then(() => res.json("this article was deleted"))
-        .catch(error => next(error));
-});
+// 	Article.findByIdAndUpdate(articleId, req.body, { new: true })
+// 		.then((updatedArticle) => {
+// 			res.json(updatedArticle)
+// 		})
+// 		.catch((error) => next(error))
+// })
 
-	Article.findByIdAndDelete(articleId)
-		.then(() => res.json('this article was deleted'))
-		.catch((error) => next(error))
-})
+// fileUploader.single('post-image'),
+// 	(req, res, next) => {
+// 		const { articleId } = req.params
 
-// UPDATE:  article
-router.put("/community/article/:articleId", fileUploader.single('post-image'), (req, res, next) => {
-    const { articleId } = req.params;
+// 		Article.findByIdAndUpdate(articleId, req.body, { new: true })
+// 			.then((updatedArticle) => {
+// 				res.json(updatedArticle)
+// 			})
+// 			.catch((error) => next(error))
+// 	},
+// 	//GET my-article
+// 	//READ: list of articles from community
+// 	router.get('/community/my-articles/:userId', (req, res, next) => {
+// 		const userId = req.params
 
-    Article.findByIdAndUpdate(articleId, req.body, { new: true })
-        .then(updatedArticle => {
-            res.json(updatedArticle);
-        })
-        .catch(error => next(error));
-});
-
-	fileUploader.single('post-image'),
-	(req, res, next) => {
-		const { articleId } = req.params
-
-		Article.findByIdAndUpdate(articleId, req.body, { new: true })
-			.then((updatedArticle) => {
-				res.json(updatedArticle)
-			})
-			.catch((error) => next(error))
-	},
-)
-
-//GET my-article
-//READ: list of articles from community
-router.get("/community/my-articles/:userId", (req, res, next) => {
-    const userId = req.params;
-
-	Article.find({ author: userId })
-		.then((articleArr) => {
-			const data = {
-				article: articleArr,
-			}
-			res.json(data)
-		})
-		.catch((err) => {
-			console.log('error getting the article', err)
-			res.status(500).json({
-				message: 'error getting the article',
-				error: err,
-			})
-		})
-})
+// 		Article.find({ author: userId })
+// 			.then((articleArr) => {
+// 				const data = {
+// 					article: articleArr,
+// 				}
+// 				res.json(data)
+// 			})
+// 			.catch((err) => {
+// 				console.log('error getting the article', err)
+// 				res.status(500).json({
+// 					message: 'error getting the article',
+// 					error: err,
+// 				})
+// 			})
+// 	})
 
 module.exports = router
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -17,4 +17,24 @@ router.get('/users/current', isAuthenticated, async (req, res) => {
 	}
 })
 
+router.put('/users/current', isAuthenticated, async (req, res) => {
+	const { id } = req.payload
+
+	try {
+		const user = await User.findById(id)
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' })
+		}
+
+		user.username = req.body.username || user.username
+		user.description = req.body.description || user.description
+
+		await user.save()
+		return res.json(user)
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ message: 'Server error' })
+	}
+})
+
 module.exports = router
